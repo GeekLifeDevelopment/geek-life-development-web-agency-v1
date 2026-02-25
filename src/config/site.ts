a5a66dbb0fe9b6e2b7e7069a949eb7c928da5ab3
Jason Sorenson <jasonsorenson@Jasons-MBP.wi.rr.com>
2026-02-24 19:19:40 -0600

    more vergbage changes

--- src/config/site.ts
+++ src/config/site.ts
@@ -2,7 +2,7 @@ export const siteConfig = {
   siteName: 'Geek Life Development',
   siteUrl: 'https://geeklifedevelopment.com',
   defaultTitle: 'Geek Life Development',
-  defaultDescription: 'Modern websites and simple apps. Serving Cedarburg & Milwaukee area + remote.',
+  defaultDescription: 'Modern websites and simple apps. Serving Ozaukee County & Milwaukee area + remote.',
   email: 'jason@geeklifedevelopment.com',
   locale: 'en_US',
   ogImage: '/og.png'
